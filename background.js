62984851be757c0c35d0c61cfd05a90eb92a85f4
noricha-vr <noricha.vr@gmail.com>
2023-02-04 11:43:54 +0800

    Change popup duration logic.

--- background.js
+++ background.js
@@ -1,5 +1,3 @@
-
-
 // Twitter の URL
 const TWITTER_URL = "https://twitter.com/";
 
@@ -7,17 +5,20 @@ const TWITTER_URL = "https://twitter.com/";
 const INITIAL_POPUP_URL = "https://www.google.com/";
 const INITIAL_DURATION = 3; // 3 分
 
+
 chrome.storage.sync.get({
 	duration: INITIAL_DURATION,
 	popupUrl: INITIAL_POPUP_URL
 }, function (items) {
-	duration = items.duration * 1000 * 60;　// 分をミリ秒に変換
+	duration = items.duration * 60;　// 分をミリ秒に変換
 	popupUrl = items.popupUrl
-	console.log(`duration: ${duration} popupUrl: ${popupUrl}`);
+	console.log(`duration: ${duration} sec. popupUrl: ${popupUrl}`);
 });
 
 // Titter のページを何回開いたかカウントする
 let twitterCount = 0;
+// Twitter のページを開いた時間を記録する
+let twitterTimer = 0;
 
 async function getUrl() {
 	let queryOptions = { active: true, currentWindow: true };
@@ -37,16 +38,27 @@ chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
 	console.log(`start url: ${url}`);
 	if (!url.startsWith(TWITTER_URL)) return;
 	twitterCount++;
-	// duration 秒後にリダイレクト
-	setTimeout(async () => {
+
+
+	let interval = setInterval(async () => {
 		let url = await getUrl();
 		console.log(`check url: ${url}`);
-		if (!url.startsWith(TWITTER_URL)) return;
-		console.log("You are still on Twitter.");
-		twitterCount = 0;
-		chrome.tabs.create({
-			url: popupUrl,
-			active: true
-		});
-	}, duration);
+		if (url.startsWith(TWITTER_URL)) {
+			console.log("You are on Twitter.");
+			twitterCount = 0;
+			if (twitterTimer < duration) {
+				twitterTimer++;
+				console.log(`twitterTimer: ${twitterTimer}`);
+			} else {
+				// duration 秒後にタブを開く
+				chrome.tabs.create({
+					url: popupUrl,
+					active: true
+				});
+				twitterTimer = 0;
+				interval = clearInterval(interval);
+			}
+
+		}
+	}, 1000);
 })	
