6cd3b2d188474e2f643cc3fbb1c7def25a480ba8
noricha-vr <noricha.vr@gmail.com>
2023-03-17 15:40:44 +0900

    Change option ui to popup.

--- options.js
+++ options.js
@@ -1,31 +1,33 @@
 // Saves options to chrome.storage
-async function save_options() {
-	var duration = document.getElementById('duration').value;
-	var popupUrl = document.getElementById('popup_url').value;
-	var status = document.getElementById('status');
+async function saveOptions() {
+	const duration = document.getElementById("duration").value;
+	const popupUrl = document.getElementById("popup_url").value;
+	const status = document.getElementById("status");
+
 	chrome.storage.sync.set({
 		duration: duration,
-		popupUrl: popupUrl
+		popupUrl: popupUrl,
 	});
-	status.classList.remove('hide');
-	await new Promise((resolve) => setTimeout(resolve, 5000));
-	status.classList.add('hide');
-	// Update status to let user know options were saved.
-	// status.classList.add('hide');
-}
 
+	status.classList.remove("hide");
+	await new Promise((resolve) => setTimeout(resolve, 5000));
+	status.classList.add("hide");
+}
 
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
-function restore_options() {
-	// Use default value limitMin = 3 and popupUrl = 'https://www.example.com/'.
-	chrome.storage.sync.get({
-		duration: 3,
-		popupUrl: 'https://www.google.com/'
-	}, function (items) {
-		document.getElementById('duration').value = items.duration;
-		document.getElementById('popup_url').value = items.popupUrl;
-	});
+function restoreOptions() {
+	chrome.storage.sync.get(
+		{
+			duration: 3,
+			popupUrl: "https://www.google.com/",
+		},
+		function (items) {
+			document.getElementById("duration").value = items.duration;
+			document.getElementById("popup_url").value = items.popupUrl;
+		}
+	);
 }
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click', save_options);
+
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.getElementById("save").addEventListener("click", saveOptions);
